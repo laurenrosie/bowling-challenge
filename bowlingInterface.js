86c68bed95f9d8c0cb6460a05503aaa936ff59f5
Lauren Rosie <laurenrosiewatson@gmail.com>
2017-02-09 01:58:29 +0000

    Updated table to show at end of frames

--- bowlingInterface.js
+++ bowlingInterface.js
@@ -1,15 +1,21 @@
 $( document ).ready(function() {
 
+  $('#show-scorecard').hide()
+  $('#scorecard').hide()
+
+
   var game = null;
   var current_roll_number = 0;
   var current_frame_number = 0;
   var currentFrame = null;
+  var currentGame = null;
 
   $("h2#frame-display").text("Frame: " + current_frame_number.toString() );
   $("h2#roll-display").text("Roll: " + current_roll_number.toString());
 
   $("button#start-game").click(function(){
-    game = new Game();
+    currentGame = new Game();
+    console.log(currentGame)
     current_roll_number = 1;
     current_frame_number = 1;
     updateStats();
@@ -27,19 +33,19 @@ $( document ).ready(function() {
     $('form#entry').submit( function(e) {
           $.ajax({
             success: function () {
-              roll = new Roll(parseInt($('input#roll-input').val()))
-              if(currentFrame!=null){
-                 frame = currentFrame
-              }else {
-                 frame = new Frame()
-
-              }
-              currentFrame = frame
-              frame.addRoll(roll)
-              current_roll_number += 1
-              console.log(roll)
-              console.log(frame)
-              updateStats()
+              if(isOver()){
+                $('form#entry').hide()
+                $('button#show-scorecard').show()
+              }else{
+                roll = new Roll(parseInt($('input#roll-input').val()))
+                currentFrame = checkFrame()
+                currentFrame.addRoll(roll)
+                current_roll_number += 1
+                console.log(roll)
+                console.log(currentFrame)
+                console.log(currentGame)
+                updateStats()
+            }
            }
        });
        e.preventDefault();
@@ -49,7 +55,33 @@ $( document ).ready(function() {
 function updateStats(){
   $("h2#frame-display").text("Frame: " + current_frame_number.toString() );
   $("h2#roll-display").text("Roll: " + current_roll_number.toString());
-
 }
+
+function checkFrame(){
+  if(currentFrame!=null&&currentFrame.rolls().length<2){
+     return currentFrame
+  }else if(currentFrame!=null&&currentFrame.rolls().length==2){
+      currentGame.addFrame(currentFrame)
+      current_frame_number+=1
+      return new Frame()
+  }else {
+    current_frame_number+=1
+     return new Frame()
+  }
+}
+
+function isOver(){
+  if(currentGame!=null&&currentGame.frames().length==10){
+    return true
+  }else{
+    return false
+  }
+}
+
+
+$("button#show-scorecard").click(function(){
+  $('button#show-scorecard').hide()
+  $('#scorecard').show()
+});
 
 });
