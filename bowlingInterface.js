0240bd64a84b6d3ffb811582931772a10c884cfb
Lauren Rosie <laurenrosiewatson@gmail.com>
2017-02-12 23:23:27 +0000

    fixed broken specs

--- bowlingInterface.js
+++ bowlingInterface.js
@@ -4,15 +4,10 @@ $( document ).ready(function() {
   $('#scorecard').hide()
   $('#the-form').hide()
 
-
   var game = null;
-  var current_roll_number = 0;
-  var current_frame_number = 0;
   var currentFrame = null;
   var currentGame = null;
 
-  $("h2#frame-display").text("Frame: " + current_frame_number.toString() );
-  $("h2#roll-display").text("Roll: " + current_roll_number.toString());
 
   $("button#start-game").click(function(){
     $('#the-form').show()
@@ -21,64 +16,79 @@ $( document ).ready(function() {
   });
 
   $("button#reset").click(function(){
-    current_roll_number = 0;
-    current_frame_number = 0;
     game = null;
     updateStats();
   });
 
-
-
     $('form#entry').submit( function(e) {
           $.ajax({
             success: function () {
-              // if(isOver()){
-              //   $('form#entry').hide()
-              //   $('#whole-main-box').hide()
-              //   $('button#show-scorecard').show()
-              // }else{
+
                 roll = new Roll(parseInt($('input#roll-input').val()))
                 currentFrame = checkFrame()
                 currentFrame.addRoll(roll)
-                current_roll_number += 1
+                // current_roll_number += 1
                 updateStats()
-            // }
-              if(isOver()){
-                $('form#entry').hide()
-                $('#whole-main-box').hide()
-                $('button#show-scorecard').show()
-              }
+
+              if(isOver()){ finalView()}
            }
        });
+
        e.preventDefault();
       });
 
 
 function updateStats(){
-  $("h2#frame-display").text("Frame: " + current_frame_number.toString() );
-  $("h2#roll-display").text("Roll: " + current_roll_number.toString());
+  currentFrameNumber = currentGame.frames().length+1
+  if(currentFrame ==null){
+    currentRollNumber=1
+  }else{
+    currentRollNumber = currentFrame.rolls().length
+  }
+
+  $("h2#frame-display").text("Frame: " + currentFrameNumber.toString() );
+  $("h2#roll-display").text("Roll: " + currentRollNumber.toString());
 }
 
+function finalView(){
+  $('form#entry').hide()
+  $('#whole-main-box').hide()
+  $('button#show-scorecard').show()
+  $('button#new-game').show()
+
+}
+
+
 function checkFrame(){
+
   if(currentFrame!=null&&currentFrame.rolls().length<2){
+
      return currentFrame
+
   }else if(currentFrame!=null&&currentFrame.rolls().length==2){
-      if(currentGame.frames().length==9 && currentFrame.isStrike()){
-        return currentFrame
-      }else{
-        currentGame.addFrame(currentFrame)
-        current_frame_number+=1
-        return new Frame()
-    }
+
+      return finalRollCase()
+
   }else {
-    if(currentFrame==0){
-     return new Frame()
-   }else{
-     current_frame_number+=1
-     return new Frame()
-   }
+    if(currentFrame==null){
+      return new Frame()
+    }else{
+      currentGame.addFrame(currentFrame)
+      return new Frame()
+    }
  }
 }
+
+function finalRollCase(){
+  if(currentGame.frames().length==9 && currentFrame.isStrike()){
+    return currentFrame
+  }else{
+    currentGame.addFrame(currentFrame)
+    return new Frame()
+  }
+
+}
+
 
 function isOver(){
   return (currentGame!=null&&currentGame.frames().length==10)
@@ -92,15 +102,16 @@ $("button#show-scorecard").click(function(){
 
 
 function fillInCard(){
+  totalsArray = currentGame.generateTotals()
+  listFrames = currentGame.frames()
   for(i=1; i<11; i++){
-    var theFrame = currentGame.frames()[i-1]
-    var theRoll = theFrame.rolls().shift()
-    var pinsScored = String(theRoll.pins())
-    var currentTotal = 0
-    $('#myTable').append("<tr><td>" + String(i) + "</td><td>"+String(1)+"</td><td>"+ pinsScored+"</td><td>"+currentTotal+"</td></tr>")
-    theRoll = theFrame.rolls().shift()
-    pinsScored = String(theRoll.pins())
-    $('#myTable').append("<tr><td>" + String(i) + "</td><td>"+String(2)+"</td><td>"+ pinsScored+"</td><td>"+currentTotal+"</td></tr>")
+    theTotals = totalsArray[i-1]
+    thisFrame = listFrames[i-1]
+    theRolls = thisFrame.rolls()
+    roll1pins = theRolls[0].pins()
+    roll2pins = theRolls[1].pins()
+    $('#myTable').append("<tr><td>" + String(i) + "</td><td>"+String(1)+"</td><td>"+ String(roll1pins)+"</td><td>"+String(theTotals[0])+"</td></tr>")
+    $('#myTable').append("<tr><td>" + String(i) + "</td><td>"+String(2)+"</td><td>"+ String(roll2pins)+"</td><td>"+String(theTotals[1])+"</td></tr>")
   }
 }
 
